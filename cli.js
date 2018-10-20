13e6bef24ac9c90b12d1b8c54a76acc28ea9727a
Max Isom <code@maxisom.me>
2018-10-20 19:06:38 -0400

    Finish formatting output.

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const Configstore = require('configstore');
 
 // Import local files
 const link = require('./lib/link');
-const listApp = require('./lib/listApp.js');
+const listApp = require('./lib/list-app.js');
 const control = require('./lib/control');
 const pkg = require('./package.json');
 
@@ -68,14 +68,13 @@ program
 
 // MITM Tuya App and list devices
 program
-  .command('list-app')
-  .description('list devices from Tuya Smart app (recommended for new users)')
-  .option('-p, --port [port]', 'port to use for proxy', 8001)
-  .option('-q, --omitQR', 'don\'t show the QR code for certificate setup', false)
-  .option('-s, --save', 'save device parameters for subsequent commands')
-  .action(options => {
-    listApp(conf, options);
-  });
+	.command('list-app')
+	.description('list devices from Tuya Smart app (recommended for new users)')
+	.option('-p, --port [port]', 'port to use for proxy', 8001)
+	.option('-q, --omitQR', 'don\'t show the QR code for certificate setup', false)
+	.action(options => {
+		listApp(conf, options);
+	});
 
 // Get help
 program
